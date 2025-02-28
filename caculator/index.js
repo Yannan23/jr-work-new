a54f296b3874f7207a88c2a20bd740b088b6db6e
Yannan23 <yannansun0723@gmail.com>
2025-03-01 09:59:27 +1100

    redo it

--- caculator/index.js
+++ caculator/index.js
@@ -1,11 +1,24 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: stdin,
-    output: stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
-const cal = () => {
+const getResult = (operator, a, b) => {
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b
+    }
+}
+
+const cal = (expression) => {
     const operators = []
     const operands = []
     const precedence = {
@@ -15,13 +28,45 @@ const cal = () => {
         '/': 2
     }
 
+    const tokens = expression.match(/\d+|[+\-*/]/g)
 
+    for (i = 0; i < tokens.length; i++) {
+        const token = tokens[i]
+        const isNumber = !isNaN(parseInt(token))
+        if (isNumber) {
+            operands.push(parseInt(token))
+        } else {
+            const hasHigherPrecedence =
+                precedence[operators[operators.length - 1]] > precedence[token]
+            while (operators.length && hasHigherPrecedence) {
+                const a = operands.pop()
+                const b = operands.pop()
+                const operator = operators.pop()
+                const result = getResult(operator, a, b)
+                operands.push(result)
+            }
+            operators.push(token)
+        }
+    }
+    while (operators.length > 0) {
+        const a = operands.pop()
+        const b = operands.pop()
+        const operator = operators.pop()
+        const result = getResult(operator, a, b)
+        operands.push(result)
+    }
+    return result = operands.pop()
 }
 
 const askQuestions = () => {
 
-    rl.question('Hello world! Please input', (expression) => {
-        console.log(`The result of ${expression} = ${cal()}`);
+    rl.question('Hello world! Please input\n', (expression) => {
+
+        if (expression === 'exit') {
+            rl.close()
+        }
+        console.log(`The result of ${expression} = ${cal(expression)}`);
     })
-    rl.close()
 }
+
+askQuestions()
